e3ee32f2cefd96134b6e15772ede58609f5a2372
[Hafs-Arif] <hafsarif99@gmail.com>
2026-04-07 01:58:38 +0500

    original backend push

--- KillsWitchBackend/routes/contantRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { handleContactRequest } = require("../controllers/contantController");
-
-
-router.post('/', handleContactRequest);
-module.exports = router;
--- KillsWitchBackend/routes/qouteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { handleQuoteRequest } = require("../controllers/qouteController");
-
-
-router.post('/', handleQuoteRequest);
-module.exports = router;
-
